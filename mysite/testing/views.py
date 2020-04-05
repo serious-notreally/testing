42de5824f2d08a5d64f104b4124cb86f6f593c30
serious-notreally <61718385+serious-notreally@users.noreply.github.com>
2020-04-05 16:36:26 +0500

    doneit

--- mysite/testing/views.py
+++ mysite/testing/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import CodePostForm
-import os
+import sqlite3
 from subprocess import Popen, PIPE
 def post_share(request):
     sent=False
@@ -8,24 +8,14 @@ def post_share(request):
         form = CodePostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            sql_code=""" 
-import sqlite3 
-import pprint 
-conn = sqlite3.connect('C:/Users/user/Documents/GitHub/testing/mysite/testing/ONLINESHOP.db') 
-c = conn.cursor()
-c.execute('''
-"""+cd['code']+""" ''')
-pprint.pprint(c.fetchall(),indent=5,width=60,compact=False) 
-c.close()
-"""
-            fl = open("C:/Users/user/Documents/GitHub/testing/mysite/testing/testingit.py", "w+")
-            fl.write(sql_code)
-            fl.close()
+
+            conn = sqlite3.connect('C:/Users/user/Documents/GitHub/testing/mysite/testing/ONLINESHOP.db')
+            c = conn.cursor()
+            c.execute(cd['code'])
             otv=""
-            result = Popen(['python', 'C:/Users/user/Documents/GitHub/testing/mysite/testing/testingit.py'], stdout=PIPE, bufsize=1)
-            for example in iter(result.stdout.readline, b''):
-                otv+=str(example)
-            os.remove('C:/Users/user/Documents/GitHub/testing/mysite/testing/testingit.py')
+            for res in c.fetchall():
+                otv+=str(res)
+            c.close()
             sent=True
             return render(request, 'share.html', {'form': form,'sent':sent,'result':otv})
         else:
